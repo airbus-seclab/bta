117516b6df8b92219a09f28d3d235f9c2a2b9344
phil <phil@secdev.org>
2013-01-30 15:50:49 +0100

    Added passwords miner

--- ntds/miners/all.py
+++ ntds/miners/all.py
@@ -1,1 +1,1 @@
-from ntds.miners import grep_dn, SDusers
+from ntds.miners import grep_dn, SDusers, passwords
---
+++ ntds/miners/passwords.py
@@ -0,0 +1,40 @@
+from ntds.miners import Miner
+from collections import defaultdict
+
+SDTABLE="sdtable"
+DATATABLE="datatable"
+
+@Miner.register
+class Passwords(Miner):
+    _name_ = "passwords"
+    _desc_ = "Look for things on user passwords"
+    @classmethod
+    def create_arg_subparser(cls, parser):
+        parser.add_argument("--bad-password-count", action="store_true", help="Find users whose bad password count is non-zero")
+        parser.add_argument("--dump-unicode-pwd", action="store_true", help="Dump unicodePwd AD field")
+    
+    def print_user(self, record, field=None):
+        fmt = "%(sAMAccountName)-20s %(name)-50s"
+        d = defaultdict(lambda:"-")
+        d.update(record)
+        if field:
+            fmt += " %%(%s)s" % field
+        print fmt % d
+    
+    def bad_password_count(self):
+        for r in self.dt.find({"badPwdCount":{"$exists": True, "$ne":"0"}}): #.sort({"badPwdCount":1}):
+            self.print_user(r, "badPwdCount")
+
+    def dump_field(self, field):
+        for r in self.dt.find({field:{"$exists": True}}):
+            self.print_user(r, field)
+            
+
+    def run(self, options):
+        
+        self.dt = options.db.db[DATATABLE]
+
+        if options.bad_password_count:
+            self.bad_password_count()
+        if options.dump_unicode_pwd:
+            self.dump_field("unicodePwd")
