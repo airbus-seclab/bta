9a91a0244c7c52e42cf1e6e3971f975c6e0dff24
Nicolas Bareil <nicolas.bareil@eads.net>
2013-03-12 11:34:36 +0100

    ListACE: objectType and Trustee

--- ntds/miners/ListACE.py
+++ ntds/miners/ListACE.py
@@ -3,6 +3,9 @@ from ntds.miners.tools import User, Group, Sid, Record
 import re
 
 
+CATEGORY_GROUP = 5945
+CATEGORY_USER  = 3818
+
 @Miner.register
 class ListACE(Miner):
     _name_ = "ListACE"
@@ -11,8 +14,8 @@ class ListACE(Miner):
     @classmethod
     def create_arg_subparser(cls, parser):
         parser.add_argument("--type", help="Look only for ACE matching TYPE", metavar="TYPE")
-        parser.add_argument("--user", help="Look only for ACE of SID", metavar="SID")
-        parser.add_argument("--target", help="Look only for ACE applying to GUID", metavar="GUID")
+        parser.add_argument("--trustee", help="Look only for ACE of SID", metavar="SID")
+        parser.add_argument("--subject", help="Look only for ACE applying to GUID", metavar="GUID")
         parser.add_argument("--noresolve", help="Do not resolve SID", action="store_true")
         parser.add_argument("--verbose", help="Show also deleted users time and RID", action="store_true")
 
@@ -20,19 +23,22 @@ class ListACE(Miner):
         r = self.dt.find_one({"rightsGuid":objtype.lower()})
         return r["name"] if r else objtype
 
-    def summarize_ace(self, acl, ace):
+    def summarize_ace(self, trustee, securitydescriptor, aceList):
         perms=[]
-        for perm, val in ace['AccessMask'].items():
-            if val:
-                perms.append(perm)
-        if 'ObjectType' in ace:
-            objtype = self.type2human(ace['ObjectType'])
-        else:
-            objtype = "No object type /!\ DANGEROUS"
-        return [str(Sid(self.dt, verbose=self.options.verbose, objectSid=acl['value']['Owner'])),
+        for ace in aceList:
+            for perm, val in ace['AccessMask'].items():
+                if val:
+                    perms.append(perm)
+            if 'ObjectType' in ace:
+                objtype = self.type2human(ace['ObjectType'])
+            else:
+                objtype = "No object type /!\ DANGEROUS"
+        return [#str(Sid(self.dt, verbose=self.options.verbose, objectSid=trustee)),
+                trustee,
                 str(Sid(self.dt, verbose=self.options.verbose, objectSid=ace['SID'])),
                 objtype,
-                ', '.join(perms)]
+                '']
+                #', '.join(perms)]
 
     def getSecurityDescriptor(self, sd_id):
         raw_securitydescriptor = self.sd.find_one({'id': sd_id})
@@ -69,50 +75,55 @@ class ListACE(Miner):
         res = []
         desc = []
         queries = []
-        if options.user:
-            users = dt.find({'objectSid': options.user})
-            desc.append("owner=%s" % options.user)
+        if options.subject:
+            users = dt.find({'objectSid': options.subject})
+            desc.append("trustee=%s" % options.subject)
 
             for raw_user in users:
                 user = Record(**raw_user)
                 securitydescriptor = self.getSecurityDescriptor(user.nTSecurityDescriptor)
                 aceList = self.extractACE(securitydescriptor)
+                filteredACE = []
                 for ace in aceList:
                     if options.type and ace.ObjectType != options.type:
                         continue
-                    if options.target and ace.SID != options.target:
+                    if options.trustee and ace.SID != options.trustee:
                         continue
-                    res.append((user, securitydescriptor, ace))
+                    filteredACE.append(ace)
+                res.append((user, securitydescriptor, filteredACE))
         else:
+            query = {}
             if options.type:
-                query = {'$or': [
-                            {"value.DACL.ACEList": {'$elemMatch': {'ObjectType': re.compile(options.type, re.IGNORECASE )}}},
-                            {"value.SACL.ACEList": {'$elemMatch': {'ObjectType': re.compile(options.type, re.IGNORECASE )}}}, ]}
-                queries.append(query)
+                query.update({'ObjectType': options.type.lower()})
                 desc.append("type=%s" % options.type)
+            if options.trustee:
+                query.update({'SID': options.trustee.upper()})
+                desc.append("trustee=%s" % options.trustee)
 
-            if options.target:
-                query = {'$or': [
-                            {"value.DACL.ACEList": {'$elemMatch': {'SID': re.compile(options.target, re.IGNORECASE )}}},
-                            {"value.SACL.ACEList": {'$elemMatch': {'SID': re.compile(options.target, re.IGNORECASE )}}}, ]}
-                queries.append(query)
-                desc.append("target=%s" % options.target)
-            bigquery = {'$and': queries}
+            bigquery = {'$or': [
+                        {"value.DACL.ACEList": {'$elemMatch': query}},
+                        {"value.SACL.ACEList": {'$elemMatch': query}}],
+                       }
 
+            desct = ("List ACE where "+ " and ".join(desc)) if desc else "List all ACE"
+            table = doc.create_table(desct)
+            table.add(["Trustee", "Subject", "Object type", "Perms"])
+            table.add()
 
-
+            entry = 0
             for raw_sd in sdt.find(bigquery):
                 securitydescriptor = Record(**raw_sd)
                 aceList = self.extractACE(securitydescriptor)
+                filteredACE = filter(lambda x: x.ObjectType == options.type, aceList)
+                trustees = set(map(lambda x: x['SID'], filteredACE))
                 users = dt.find({'nTSecurityDescriptor': securitydescriptor.id})
                 for user in users:
-                    res.append((user, securitydescriptor, ace))
-
-        desct = ("List ACE where "+ " and ".join(desc)) if desc else "List all ACE"
-        table = doc.create_table(desct)
-        table.add(["Owner", "Object", "Object type", "Perms"])
-        table.add()
-        for t in res:
-            user, securitydescriptor, ace = t
-            table.add(self.summarize_ace(securitydescriptor, ace))
-        table.finished()
+                    cat = int(user.get('objectCategory', 0))
+                    if cat not in [CATEGORY_USER, CATEGORY_GROUP]:
+                        continue
+                    entry += 1
+                    #trustees = map(lambda x:  str(Sid(self.dt, verbose=self.options.verbose, objectSid=x)), trustees)
+                    table.add([', '.join(trustees),
+                              str(Sid(self.dt, verbose=self.options.verbose, objectSid=user['objectSid'])),
+                              None, None])
+            table.finished()
