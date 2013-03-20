0dc5e2670470d63355ee0b4314ab793e0432a837
Nicolas Bareil <nicolas.bareil@eads.net>
2013-03-20 15:51:37 +0100

    lsgroup devient recursif

--- ntds/miners/ListGroup.py
+++ ntds/miners/ListGroup.py
@@ -1,11 +1,12 @@
 from ntds.miners import Miner
-from collections import defaultdict
 from ntds.miners.tools import User, Group, Sid
+
 
 @Miner.register
 class ListGroup(Miner):
     _name_ = "ListGroup"
     _desc_ = "List group membership"
+    groups_already_saw={}
 
     @classmethod
     def create_arg_subparser(cls, parser):
@@ -13,9 +14,34 @@ class ListGroup(Miner):
         parser.add_argument("--noresolve", help="Do not resolve SID", action="store_true")
         parser.add_argument("--verbose", help="Show also deleted users time and RID", action="store_true")
 
+    def get_members_of(self, sid):
+        group = self.dt.find_one({ 'objectSid': sid})
+        if not group:
+            return set()
+        members=set()
+        for link in self.lt.find({'link_DNT': group['RecId']}):
+            deleted=False
+            if 'link_deltime' in link and link['link_deltime'].year > 1970:
+                deleted = link['link_deltime']
+            row = self.dt.find_one({'RecId': link['backlink_DNT'] })
+            if not row:
+                members.add('[no entry %d found]' % link['backlink_DNT'])
+                continue
+            sid = row['objectSid']
+            if row['objectCategory'] == '5945':
+                if sid not in self.groups_already_saw:
+                    members.update(self.get_members_of(sid))
+                    self.groups_already_saw[sid] = True
+            elif row['objectCategory'] == '3818':
+                membership = (row['objectSid'], deleted)
+                members.add(membership)
+            else:
+                print '***** Unknown category (%s) for %s' % (row['objectCategory'], sid)
+        return members
+
     def run(self, options, doc):
-        dt = options.backend.open_table("datatable")
-        lt = options.backend.open_table("linktable")
+        self.dt = dt = options.backend.open_table("datatable")
+        self.lt = lt = options.backend.open_table("linktable")
         match = None
         
         doc.add("List of groups matching [%s]" % options.match)
@@ -30,12 +56,7 @@ class ListGroup(Miner):
         groups={}
         for group in dt.find(match):
              groups[group['objectGUID']]=set()
-             for link in lt.find({'link_DNT': group['RecId']}):
-                 deleted=False
-                 if 'link_deltime' in link and link['link_deltime'].year > 1970:
-                     deleted = link['link_deltime']
-                 membership = (User(dt, RecId=link['backlink_DNT']), deleted)
-                 groups[group['objectGUID']].add(membership)
+             groups[group['objectGUID']] = self.get_members_of(group['objectSid'])
 
         misc=''
         glist = doc.create_list("")
@@ -43,12 +64,9 @@ class ListGroup(Miner):
             group = Sid(dt, objectGUID=groupname, verbose=options.verbose)
             glist.add(group)
             sublist = glist.create_list("")
-            for member,deleted in membership:
-                if options.noresolve:
-                    member = member.objectSid
-                else:
-                    sid = Sid(dt, objectSid=member.objectSid)
-                    member = str(sid)
+            for sid,deleted in membership:
+                sidobj = Sid(dt, objectSid=sid)
+                member = str(sidobj)
                 if options.verbose and deleted:
                     member += " deleted %s" % deleted
                 sublist.add('{0} {1}'.format(member, misc))
