b662acec214cd4f4d070d2c7687f4f630c76e953
root <root@debian>
2013-03-28 17:30:35 +0100

    Patch for #7

--- bta/miners/ListGroup.py
+++ bta/miners/ListGroup.py
@@ -2,7 +2,6 @@
 from bta.miners import Miner
 from bta.miners.tools import User, Group, Sid, CATEGORY_GROUP, CATEGORY_USER
 
-
 @Miner.register
 class ListGroup(Miner):
     _name_ = "ListGroup"
@@ -16,7 +15,7 @@ class ListGroup(Miner):
         parser.add_argument("--verbose", help="Show also deleted users time and RID", action="store_true")
 
     def get_members_of(self, grpsid, recursive=False):
-        group = self.dt.find_one({ 'objectSid': grpsid})
+        group = self.dt.find_one({'objectSid': grpsid})
         if not group:
             return set()
         members=set()
@@ -24,7 +23,7 @@ class ListGroup(Miner):
             deleted=False
             if 'link_deltime' in link and link['link_deltime'].year > 1970:
                 deleted = link['link_deltime']
-            row = self.dt.find_one({'RecId': link['backlink_DNT'] })
+            row = self.dt.find_one({'RecId': link['backlink_DNT']})
             if not row:
                 members.add('[no entry %d found]' % link['backlink_DNT'])
                 continue
@@ -41,6 +40,18 @@ class ListGroup(Miner):
             else:
                 print '***** Unknown category (%d) for %s' % (category, sid)
         return members
+        
+    def getInfo_fromSid(self, sid):
+        return self.dt.find_one({'objectSid': sid})
+    
+    def find_dn(self, r):
+        if not r:
+            return ""
+        cn = r.get("cn") or r.get("name")
+        if cn is None or cn=="$ROOT_OBJECT$":
+            return ""
+        r2 = self.dt.find_one({"RecId":r["ParentRecId"]})
+        return self.find_dn(r2)+"."+cn
 
     def run(self, options, doc):
         def deleted_last(l):
@@ -52,12 +63,12 @@ class ListGroup(Miner):
                     deleteditems.append(i)
             for i in deleteditems:
                 yield i
+                
         self.dt = dt = options.backend.open_table("datatable")
         self.lt = lt = options.backend.open_table("linktable")
         match = None
         
         doc.add("List of groups matching [%s]" % options.match)
-        
         if options.match:
             match = {"$and": [{'objectCategory': str(CATEGORY_GROUP)},
                               {"$or": [ { "name": { "$regex": options.match } },
@@ -67,15 +78,26 @@ class ListGroup(Miner):
 
         groups={}
         for group in dt.find(match):
-             groups[group['objectSid']]=set()
-             groups[group['objectSid']] = self.get_members_of(group['objectSid'])
+            groups[group['objectSid']] = set()
+            groups[group['objectSid']] = self.get_members_of(group['objectSid'])
 
-        misc=''
         headers=['User', 'Deletion', 'Flags', 'Recursive']
-
-        for groupname,membership in groups.items():
-            group = Sid(dt, objectSid=groupname, verbose=True)
-            table = doc.create_table("Members of %s" % group)
+        
+        listemptyGroup=[]
+        for groupSid,membership in groups.items():
+            if len(membership) == 0: 
+                listemptyGroup.append(groupSid)
+                continue
+                
+            info = self.getInfo_fromSid(groupSid)
+            name = info['cn']
+            guid = info['objectGUID']
+            
+            sec = doc.create_subsection("Group %s" % name)
+            sec.add("sid = %s" % groupSid)
+            sec.add("guid= %s" % guid)
+            sec.add("cn  = %s" % self.find_dn(info))
+            table = sec.create_table("Members of %s" % name)
             table.add(headers)
             table.add()
             for sid,deleted,fromgrp in deleted_last(membership):
@@ -86,3 +108,17 @@ class ListGroup(Miner):
                 flags = sidobj.getUserAccountControl()
                 table.add((member, deleted or '', flags, fromgrp))
             table.finished()
+            sec.finished()
+        
+        if len(listemptyGroup) > 0:
+            headers=['Group', 'SID', 'Guid']
+            table = doc.create_table("Empty groups")
+            table.add(headers)
+            table.add()
+            for groupSid in listemptyGroup:
+                info = self.getInfo_fromSid(groupSid)
+                name = info['cn']
+                guid = info['objectGUID']
+                table.add((name, groupSid, guid))
+            table.finished()
+        
