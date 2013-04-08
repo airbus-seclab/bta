376db8d735d793e44ceab5456c9e44b41ec6ba13
phil <phil@secdev.org>
2013-04-08 18:05:47 +0200

    Added Registry class in tool
    Registry can be used to keep a list of functions thanks to a decorator

--- bta/tools.py
+++ bta/tools.py
@@ -1,4 +1,5 @@
 import struct
+from collections import defaultdict
 
 def decode_sid(s, endianness="<"):
     "Depending on the source, last sub-authority will be little or big endian"
@@ -18,3 +19,30 @@ def decode_guid(s):
     part1 =  "%08x-%04x-%04x-" % struct.unpack("<IHH", s[:8])
     part2 = "%04x-%08x%04x" % struct.unpack(">HIH", s[8:])
     return part1+part2
+
+
+class Registry(object):
+    registry = defaultdict(dict)
+    @classmethod
+    def register(cls, **kargs):
+        def do_reg(f, cls=cls, kargs=kargs):
+            cls.registry[cls.__name__][f.__name__]=kargs
+            return f
+        return do_reg
+    
+    @classmethod
+    def get_all(cls):
+        return cls.registry[cls.__name__]
+    @classmethod
+    def get(cls, name, default=None):
+        return cls.registry[cls.__name__].get(name, default)
+    
+    @classmethod
+    def iterkeys(cls):
+        return iter(cls.registry[cls.__name__])
+    @classmethod
+    def iteritems(cls):
+        return cls.registry[cls.__name__].iteritems()
+    @classmethod
+    def itervalues(cls):
+        return cls.registry[cls.__name__].itervalues()
