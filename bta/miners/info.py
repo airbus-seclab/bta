9500ec00e8e6a35a0d1f8211ff59ff163317d613
phil <phil@secdev.org>
2013-04-18 01:51:38 +0200

    Added dump of dblog table into info miner

--- bta/miners/info.py
+++ bta/miners/info.py
@@ -8,21 +8,40 @@ class Info(Miner):
     @classmethod
     def create_arg_subparser(cls, parser):
         parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
+
+    def __init__(self, backend):
+        Miner.__init__(self, backend)
+        self.dblog = backend.open_table("log")
     
     def run(self, options, doc):
-        tbl = doc.create_table("datatable")
+        s1 = doc.create_subsection("logs")
+        lst = s1.create_list("logs")
+
+        c = self.dblog.find()
+        for entry in c:
+            lst.add("%(date)s: %(args)s" % entry)
+            lst2 = lst.create_list("actions")
+            for a in entry["actions"]:
+                lst2.add("%(date)s: %(action)s" % a)
+            lst2.finished()
+        lst.finished()
+        s1.finished()
+
+        s2 = doc.create_subsection("tables")
+
+        tbl = s2.create_table("datatable")
         tbl.add(("number of records", self.datatable.count()))
         tbl.add(("number of columns", self.datatable_meta.count()))
         tbl.finished()
-        tbl = doc.create_table("sd_table")
+        tbl = s2.create_table("sd_table")
         tbl.add(("number of records", self.sd_table.count()))
         tbl.finished()
-        tbl = doc.create_table("link_table")
+        tbl = s2.create_table("link_table")
         tbl.add(("number of records", self.link_table.count()))
         tbl.finished()
 
         if options.verbose:
-            tbl = doc.create_table("datatable columns")
+            tbl = s2.create_table("datatable columns")
             tbl.add(("name", "attname", "type", "number of records"))
             tbl.add()
             for r in self.datatable_meta.find():
@@ -30,7 +49,7 @@ class Info(Miner):
             tbl.finished()
         
         if options.verbose:
-            tbl = doc.create_table("categories")
+            tbl = s2.create_table("categories")
             tbl.add(("id", "name", "number of records"))
             tbl.add()
             for r in self.category.find():
