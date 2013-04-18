da5f2333d8cf8e0b42e2f54f43ce3ca813f904dc
phil <phil@secdev.org>
2013-04-18 02:07:33 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'bta',
-    version = '0.1',
+    version = '0.2',
     packages=['bta', 'bta/backend', 'bta/miners', 'bta/formatters', 
               'libesedb'],
     scripts = ['bin/miners', 'bin/ntds2db'],
