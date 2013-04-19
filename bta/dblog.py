ad40ca93f9a5008a44268fb8d1fd78561f60b864
phil <phil@secdev.org>
2013-04-19 16:20:09 +0200

    Added ability to use DBLog instance as a context manager

--- bta/dblog.py
+++ bta/dblog.py
@@ -3,12 +3,29 @@
 import sys
 import datetime
 import pkg_resources
+import contextlib
 
 class DBLogEntry(object):
     def __init__(self, backend):
         self.backed = backend
         self.log = backend.open_table("log")
         self.log.ensure_created()
+
+    @classmethod
+    @contextlib.contextmanager
+    def dblog_context(cls, backend):
+        dblog = cls(backend)
+        dblog.create_entry()
+        try:
+            yield dblog
+        except KeyboardInterrupt:
+            dblog.update_entry("Interrupted by user (Ctrl-C)")
+            raise
+        except Exception,e:
+            dblog.update_entry("ERROR: %s" % e)
+            raise
+        else:
+            dblog.update_entry("Graceful exit")
 
     def create_entry(self):
         e = dict(
