2a29f124599b7d4e7048c961701511a26aea4a63
Nicolas <nicolas.striebig@cyberactionlab.net>
2013-10-29 14:30:31 +0100

    Splitting Flags and Enumns in a new file

--- bta/sd.py
+++ bta/sd.py
@@ -3,6 +3,7 @@
 
 import struct
 import tools.decoding
+from tools.flags import Flags, Enums
 
 # Heavily inspired by reading WinNT.h
 
@@ -49,41 +50,6 @@ class ACE(object):
         type,flags,size = struct.unpack_from("<BBH", ace)
         
     
-
-
-class Flags(object):
-    _flags_ = {}
-    def __init__(self, flags):
-        self.flags = flags
-
-    def test_flag(self, f):
-        return bool(self.flags & f == f)
-
-    def __getattr__(self, attr):
-        if attr in self._flags_:
-            return self.test_flag(self._flags_[attr])
-        raise AttributeError(attr)
-
-    def to_json(self):
-        j = {}
-        for k,v in self._flags_.iteritems():
-            j[k] = self.test_flag(v)
-        return j
-    
-        
-
-
-class Enums(object):
-    def __init__(self, val):
-        renum = {}
-        for k,v in self._enum_.iteritems():
-            renum[v] = k
-        self.renum = renum
-        self.val = val
-        self.text = self.renum.get(val, "unk:%r" % val)
-    def to_json(self):
-        return self.text
-        
 
 class ACEType(Enums):
     _enum_ = {
