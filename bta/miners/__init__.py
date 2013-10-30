2bc6f1e42013b71351e0ced4176223b504981646
Nicolas <nicolas.striebig@cyberactionlab.net>
2013-10-30 16:27:29 +0100

    Adding a new miner to check accounts with admincount but no high privileges

--- bta/miners/__init__.py
+++ bta/miners/__init__.py
@@ -3,5 +3,5 @@
 
 __all__ = [ "info", "grep_dn", "SDusers", "passwords", "MailBoxRights", "Membership", 
             "ListGroup", "ListACE", "WhoIs", "SDProp", "Schema", "NewAdmin", "ListObject",
-	        "tree_dn", "NoPassword",
+	        "tree_dn", "NoPassword", "AdminCountCheck",
             ]
