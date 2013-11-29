35b1b2a3800e285dc2a88e78af243451a224a6ef
Nicolas <nicolas.striebig@cyberactionlab.net>
2013-11-29 14:56:40 +0100

    Adding some tools for class searching and tree generation

--- bta/miners/tools.py
+++ bta/miners/tools.py
@@ -2,18 +2,19 @@
 # (c) EADS CERT and EADS Innovation Works
 
 import os, sys
+from bta.tools.WellKnownSID import SID2StringFull
 
 load_rid_path=os.path.join(os.environ['HOME'], 'local_rid.py')
 local_relative_domains_sid=None
 
 class Family(object):
     @classmethod
-    def find_siblings(cls, node, datatable):
-        siblings=list()
-        id_siblings=[s["DNT_col"] for s in datatable.find({"PDNT_col":node['DNT_col']},{"DNT_col":1})]
-        for i in id_siblings:
-            siblings.append(datatable.find({"DNT_col":i}).limit(1)[0])
-        return siblings
+    def find_childs(cls, node, datatable):
+        childs=list()
+        id_childs=[s["DNT_col"] for s in datatable.find({"PDNT_col":node['DNT_col']},{"DNT_col":1})]
+        for i in id_childs:
+            childs.append(datatable.find({"DNT_col":i}).limit(1)[0])
+        return childs
 
     @classmethod
     def find_parents(cls, node, datatable):
@@ -22,7 +23,107 @@ class Family(object):
             parents.append(datatable.find({"DNT_col":a}).limit(1)[0])
         return parents
 
+    @classmethod
+    def find_offspring(cls, node, datatable, rec=1, need=['name','DNT_col']):
 
+        def needed(node, stuffs=need):
+            collected=list()
+            for a in stuffs:
+                try:
+                    collected.append(str(node[a]))
+                except:
+                    collected.append("X")
+            return ":".join(collected)
+
+        def find_rec(node, offspring, datatable, rec):
+            childs=Family.find_childs(node, datatable)
+            id_dict=needed(node)
+            for s in childs:
+                id_new_node=needed(s)
+                if (len(Family.find_childs(s, datatable))==0 or rec==0):
+                    offspring[u"leafs"].append(id_new_node)
+                else:
+                    #if u'%s:%s'%(node['name'],node['DNT_col']) not in offspring.keys():
+                    offspring[id_new_node]={u"leafs":list()}
+                    find_rec(s, offspring[id_new_node] , datatable, rec-1)
+            return offspring
+        init={needed(node):find_rec(node, {u"leafs":list()}, datatable, rec)}
+        #from pprint import pprint
+        #pprint(init)
+        return init
+
+    @classmethod
+    def correlate(cls, tree, criteria, doc, datatable):
+    # Criteria is a list of couples (<position> and <dict>)
+    # The position is the one in the key of tree where each value is ':' separated
+    # The correlate function searh if the word in the tree key <position> is in the list formed by all <dict> keys
+        def check_eligibility(criteria, l):
+            ok=0
+            for (lucky_position,lucky_nodes) in criteria:
+                if u"%s"%(l.split(":")[lucky_position]) in lucky_nodes.keys():
+                    ok+=1
+            return ok==len(criteria)
+        dico=None
+        for k,v in tree.items():
+            #I check if the key matches if it's not leafs
+            if (k!="leafs" and check_eligibility(criteria, k)):
+                dico=doc.create_list(k)
+                for (lucky_position,lucky_nodes) in criteria:
+                    for info in lucky_nodes[u"%s"%k.split(":")[lucky_position]]:
+                        dico.add(info)
+            #I check values
+            if type(v)==list:
+                for l in v:
+                    if check_eligibility(criteria, l):
+                        ri = doc.create_list(l)
+                        for (lucky_position,lucky_nodes) in criteria:
+                            for info in lucky_nodes[u"%s"%l.split(":")[lucky_position]]:
+                                ri.add(info)
+                            ri.finished()
+                    else:
+                        doc.add(l)
+            elif type(v)==dict:
+                if dico is None:
+                    dico=doc.create_list(k)
+                Family.correlate(v,criteria,dico,datatable)
+                dico.finished()
+
+    @classmethod
+    def find_the_one(cls, cn, datatable):
+        steps=cn.split(":")
+        the_node=None
+        nodes = datatable.find({"name":steps[-1]})
+        for node in nodes:
+            ancestors=Family.find_parents(node, datatable)
+            if ["$ROOT_OBJECT$\x00"]+steps == [a['name'] for a in ancestors]:
+                the_node=node
+                break
+        return the_node
+
+class ObjectClass(object):
+    @classmethod
+    def find_my_class(cls, node, datatable):
+        classes = datatable.find({"DNT_col": node["DNT_col"]},{objectClass:1})
+        return classes
+
+    @classmethod
+    def instanceOfClass(cls, searchedGovernID, datatable):
+        all_instances = datatable.find({"objectClass":{"$in":[u'%s'%searchedGovernID]}})
+        results=dict()
+        for i in all_instances:
+            results[u'%s'%i["DNT_col"]]=["----------"]
+        return results
+
+    @classmethod
+    def find_my_possuperiors(cls, name, datatable):
+        possSuperiors = datatable.find_one({"name":name, "systemPossSuperiors":{"$exists":True}},{"systemPossSuperiors":1})
+        if possSuperiors is not None:
+            return possSuperiors.get("systemPossSuperiors")
+        return []
+
+    @classmethod
+    def test():
+        return ""
 
 class Sid(object):
     def __init__(self, sid, dt):
