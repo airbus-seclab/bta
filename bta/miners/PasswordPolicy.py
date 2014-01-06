1d9a00f08cd5b7007f8e88b6e5fac9151dba3752
Nicolas <nicolas.striebig@cyberactionlab.net>
2014-01-06 16:08:05 +0100

    Improving the miner to follow GPlinks

--- bta/miners/PasswordPolicy.py
+++ bta/miners/PasswordPolicy.py
@@ -4,6 +4,7 @@
 from bta.miner import Miner
 from datetime import datetime, timedelta
 from bta.miners.tools import Family
+from bta.tools.WellKnownSID import SID2StringFull
 import re
 
 @Miner.register
@@ -25,28 +26,49 @@ class PasswordPolicy(Miner):
     def extractGpLinks(self, gp_attr):
         regex="\[LDAP://(?P<CN>[^;]*);(?P<activated>\d)\]"
         m=re.findall(regex, gp_attr)
-        res=""
-        for link in m:
-            res+="%s %s "% (link[0], link[1])
-        print self.dnames.find({"DName":{"$regex":link[0],"$options":"i"}},{"DNT_col":1})
+        res=list()
+        for (link_DN, activation) in m:
+            convertion = self.dnames.find_one({"DName":{"$regex":"^%s"%link_DN,"$options":"i"}})
+            res.append((link_DN, convertion, activation))
         return res
     
     def run(self, options, doc):
         if not options.cn:
-            print "the root is mandatory"
+            print "The root is mandatory, you need to specify the \"cn\" argument"
             exit(1)
         the_node=Family.find_the_one(options.cn, self.datatable)
         t = doc.create_list("The password policy for the domain %s is" % options.cn)
+        gplinks=list()
+        # Display all attributes in _ListOfAttributes_
         for att in self._ListOfAttributes_:
-            att_value="Not set !"
+            att_value=""
             if (att in the_node.keys()): 
                 if isinstance(the_node[att],datetime):
                     att_value = str(the_node[att]-datetime.fromtimestamp(0))
-                elif att=="gPLink":
-                    att_value = self.extractGpLinks(the_node[att])
                 else:
                     att_value = the_node[att] 
-            t.add("the attribute %s has the value %s"%(att, att_value))
+                if att == "gPLink":
+                    tt = t.create_list("The attribute %s contains"%att)
+                    gplinks = self.extractGpLinks(the_node[att])
+                    for l in gplinks:
+                        tt.add("%s %s"%(l[0],l[2]))
+                else:
+                    t.add("The attribute %s has the value %s"%(att, att_value))
+        t.add()
+        # Find the ACL of each gplink
+        for gplink in gplinks:
+            the_link = self.datatable.find_one({"DNT_col":gplink[1]["DNT_col"]})
+            the_link_acl = self.sd_table.find_one({"sd_id":the_link["nTSecurityDescriptor"]})
+            ttt=doc.create_list("The ACL of the group policy container %s are" % gplink[0])
+            for ace in the_link_acl["sd_value"]["DACL"]["ACEList"]:
+                tttt=ttt.create_list("%s has the right %s on %s"%(SID2StringFull(ace["SID"],self.guid),
+                                                                  ace["Type"],
+                                                                  SID2StringFull(ace.get("ObjectType","everything"),self.guid,only_converted=True))) 
+                for flag in [a for a,b in ace["AccessMask"]["flags"].items() if b]:
+                    tttt.add("%s"%flag)
+
+            ttt.add()
+
         t.flush()
         t.finished()
         
