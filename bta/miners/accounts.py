118d15a959c5ab8c4e9fbeeb5b44738a40bba01d
Nicolas <nicolas.striebig@cyberactionlab.net>
2014-01-08 11:17:45 +0100

    Adding account specification Person or Computer

--- bta/miners/accounts.py
+++ bta/miners/accounts.py
@@ -10,11 +10,17 @@ from datetime import datetime
 class Passwords(Miner):
     _name_ = "accounts"
     _desc_ = "Look for things on user passwords"
+    _types_= ["Person", "Computer"]
+
     @classmethod
     def create_arg_subparser(cls, parser):
         parser.add_argument("--created-since", nargs='?', const=-1, type=int, help="List account created from X days (no argument for listing)")
         parser.add_argument("--changed-since", nargs='?', const=-1, type=int, help="List account changed from X days (no argument for listing)")
         parser.add_argument("--deleted-since", nargs='?', const=-1, type=int, help="List account deleted from X days (no argument for listing)")
+        parser.add_argument("--expire-since", nargs='?', const=-1, type=int, help="List account deleted from X days (no argument for listing)")
+        parser.add_argument("--owners", nargs='?', const="", type=str, help="List account owners (no argument for listing, regex matching when specified)")
+        parser.add_argument("--account-type", nargs='?', const=cls._types_[0], type=str, help="(%s)"%', '.join(cls._types_))
+
     
     def get_line(self, record, line):
     	res = [record.get(x,"-") if type(record.get(x,"-")) in [unicode,int,datetime] else unicode(str(record.get(x,"-")), errors='ignore').encode('hex') for x in line]
@@ -23,7 +29,7 @@ class Passwords(Miner):
 
     def extract_field_since(self, doc, field, since, types):
         results=list()
-        for r in self.datatable.find({field:{"$exists": True},"objectClass":{"$in":types}}):
+        for r in self.datatable.find({field:{"$exists": True},"objectCategory":{"$in":types}}):
             if ( (datetime.now()-r[field]).days >= since or since<0):
                 results.append(self.get_line(r, ["name", field]))
 
@@ -47,7 +53,7 @@ class Passwords(Miner):
 
     def extract_advanced_field_since(self, doc, field, since, types):
         results=list()
-        for r in self.datatable.find({"replPropertyMetaData.OID":field, "objectClass":{"$in":types}}):
+        for r in self.datatable.find({"replPropertyMetaData.OID":field, "objectCategory":{"$in":types}}):
             the_date = self.extract_replPropertyMetaData(r,field)["date"]
             if ( (datetime.now()-the_date).days >= since or since<0):
                 results.append([r["name"], the_date, SID2StringFull(r["objectSid"], self.guid)])
@@ -63,16 +69,39 @@ class Passwords(Miner):
             for r in results:
                 t.add(r)
 
+    def extract_owner(self, doc, account, types):
+        t = doc.create_table("Dump of accounts %s owners" % account)
+        t.add(["account", "owner"])
+        t.add()
+        req = {"objectCategory":{"$in":types}}
+        if account != "":
+            req.update({"name":{"$regex":account, "$options":"-i"}})
+        for r in self.datatable.find(req,{"nTSecurityDescriptor":1, "objectSid":1}):
+            infos = self.sd_table.find_one({"sd_id":r["nTSecurityDescriptor"]},{"sd_value.Owner":1})
+            t.add([SID2StringFull(r["objectSid"],self.guid), SID2StringFull(infos["sd_value"]["Owner"], self.guid)])
+        t.flush()
+
     def run(self, options, doc):
 
+        if options.account_type not in self._types_:
+            account_type=self.datatable.find_one({"name":self._types_[0] })["DNT_col"]
+        else:
+            account_type=self.datatable.find_one({"name":options.account_type})["DNT_col"]
+
         if options.created_since is not None:
-            self.extract_field_since(doc, "whenCreated", options.created_since, ["2.5.6.7"])
+            self.extract_field_since(doc, "whenCreated", options.created_since, [account_type])
     
         if options.changed_since is not None:
-            self.extract_field_since(doc, "whenChanged", options.created_since, ["2.5.6.7"])
+            self.extract_field_since(doc, "whenChanged", options.created_since, [account_type])
+
+        if options.expire_since is not None:
+            self.extract_field_since(doc, "accountExpires", options.created_since, [account_type])
+
+        if options.owners is not None:
+            self.extract_owner(doc, options.owners, [account_type])
 
         if options.deleted_since is not None:
-            self.extract_advanced_field_since(doc, "1.2.840.113556.1.2.48", options.deleted_since, ["2.5.6.7"])
+            self.extract_advanced_field_since(doc, "1.2.840.113556.1.2.48", options.deleted_since, [account_type])
 
     def assert_consistency(self):
         Miner.assert_consistency(self)
