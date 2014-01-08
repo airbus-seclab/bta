1f903a9018f546588fd321b82affa2a0bcde9a0f
Nicolas <nicolas.striebig@cyberactionlab.net>
2014-01-08 11:06:44 +0100

    Adding account type selection Person or Computer

--- bta/miners/passwords.py
+++ bta/miners/passwords.py
@@ -10,6 +10,8 @@ from datetime import datetime
 class Passwords(Miner):
     _name_ = "passwords"
     _desc_ = "Look for things on user passwords"
+    _types_= ["Person", "Computer"]
+
     @classmethod
     def create_arg_subparser(cls, parser):
         parser.add_argument("--bad-password-count",action="store_true", help="Find users whose bad password count is non-zero")
@@ -19,6 +21,7 @@ class Passwords(Miner):
         parser.add_argument("--failed-logon", nargs='?', const=-1, type=int, help="List failed authentication since X days (No argument for listing)")
         parser.add_argument("--account-creation", action="store_true", help="List all account creation dates")
         parser.add_argument("--never-logged", action="store_true", help="List all account never used")
+        parser.add_argument("--account-type", nargs='?', const=cls._types_[0], type=str, help="(%s)"%', '.join(cls._types_))
     
     def get_line(self, record, line):
     	res = [record.get(x,"-") if type(record.get(x,"-")) in [unicode,int,datetime] else unicode(str(record.get(x,"-")), errors='ignore').encode('hex') for x in line]
@@ -33,19 +36,19 @@ class Passwords(Miner):
             t.add(self.get_line(r, ["name", field]))
             t.flush()
 
-    def account_creation_date(self, doc):
+    def account_creation_date(self, doc, account_type):
         t = doc.create_table("Account creation date")
         t.add(["name", "whenCreated", "comments"])
         t.add()
-        for account in self.datatable.find({"whenCreated":{"$exists":True}, 
-                                            "objectClass":{"$in":["2.5.6.7"]}, 
+        for account in self.datatable.find({"whenCreated":{"$exists":True},
+                                            "objectCategory":{"$in":[account_type]}, 
                                             "$or":[{"isDeleted":False}, {"isDeleted":{"$exists":False}}]}):
             t.add(self.get_line(account, ["name", "whenCreated"]))
             t.flush()
 
-    def extract_field_since(self, doc, field, since, invert=False):
+    def extract_field_since(self, doc, field, since, account_type, invert=False):
         results=list()
-        for r in self.datatable.find({field:{"$exists": True}}):
+        for r in self.datatable.find({field:{"$exists": True},"objectCategory":{"$in":[account_type]}}):
             cond = (datetime.now()-r[field]).days >= since
             if invert:
                 cond = not cond
@@ -64,18 +67,25 @@ class Passwords(Miner):
                 t.add(r)
                 t.flush()
 
-    def never_logged(self, doc, field):
+    def never_logged(self, doc, field, account_type):
         t = doc.create_table("Dump of %s" % field)
         t.add(["name"])
         t.add()
-        for account in self.datatable.find({field:{"$exists":False}, 
-                                            "objectClass":{"$in":["2.5.6.7"]}, 
+        for account in self.datatable.find({field:{"$exists":False},
+                                            "objectCategory":{"$in":[account_type]}, 
                                             "$or":[{"isDeleted":False}, {"isDeleted":{"$exists":False}}]}):
             t.add(self.get_line(account, []))
             t.flush()
 
 
     def run(self, options, doc):
+
+
+        if options.account_type not in self._types_:
+            account_type=self.datatable.find_one({"name":self._types_[0] })["DNT_col"]
+        else:
+            account_type=self.datatable.find_one({"name":options.account_type})["DNT_col"]
+        print account_type
 
         if options.bad_password_count:
             self.dump_field(doc, "badPwdCount")
@@ -88,16 +98,16 @@ class Passwords(Miner):
             self.extract_field_since(doc, "pwdLastSet", abs(arg), arg<0)
 
         if options.last_logon is not None:
-            self.extract_field_since(doc, "lastLogonTimestamp", options.last_logon)
+            self.extract_field_since(doc, "lastLogonTimestamp", options.last_logon, account_type)
 
         if options.account_creation:
-            self.account_creation_date(doc)
+            self.account_creation_date(doc, account_type)
 
         if options.dump_unicode_pwd:
             self.dump_field(doc, "unicodePwd")
 
         if options.never_logged:
-            self.never_logged(doc, "lastLogonTimestamp")
+            self.never_logged(doc, "lastLogonTimestamp", account_type)
     
     def assert_consistency(self):
         Miner.assert_consistency(self)
