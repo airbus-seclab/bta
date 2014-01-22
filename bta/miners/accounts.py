a2b3442dd5eaaa5714e94c132bebb29e4e535f9f
Nicolas <nicolas.striebig@cyberactionlab.net>
2014-01-22 09:32:16 +0100

    Fixing the mongo request

--- bta/miners/accounts.py
+++ bta/miners/accounts.py
@@ -35,7 +35,7 @@ class Passwords(Miner):
 
         t = doc.create_table("Dump of %s" % field)
 
-        if len(results)==0:
+        if len(results) == 0:
             t.add(["No Result"])
             return
         else:
@@ -46,21 +46,21 @@ class Passwords(Miner):
                 t.flush()
 
     def extract_replPropertyMetaData(self, node, OID):
-        for r in node.get("replPropertyMetaData",list()):
-            if r["OID"]==OID:
+        for r in node.get("replPropertyMetaData", list()):
+            if r["OID"] == OID:
                 return r
         return list()
 
     def extract_advanced_field_since(self, doc, field, since, types):
-        results=list()
-        for r in self.datatable.find({"replPropertyMetaData.OID":field, "objectCategory":{"$in":types}}):
-            the_date = self.extract_replPropertyMetaData(r,field)["date"]
-            if ( (datetime.now()-the_date).days >= since or since<0):
+        results = list()
+        for r in self.datatable.find({"replPropertyMetaData.OID": field, "objectCategory": {"$in": types}}):
+            the_date = self.extract_replPropertyMetaData(r, field)["date"]
+            if (datetime.now() - the_date).days >= since or since < 0:
                 results.append([r["name"], the_date, SID2StringFull(r["objectSid"], self.guid)])
 
         t = doc.create_table("Dump of %s" % field)
 
-        if len(results)==0:
+        if len(results) == 0:
             t.add(["No Result"])
             return
         else:
@@ -73,20 +73,20 @@ class Passwords(Miner):
         t = doc.create_table("Dump of accounts %s owners" % account)
         t.add(["account", "owner"])
         t.add()
-        req = {"objectCategory":{"$in":types}}
+        req = {"objectCategory": {"$in": types}}
         if account != "":
-            req.update({"name":{"$regex":account, "$options":"-i"}})
-        for r in self.datatable.find(req,{"nTSecurityDescriptor":1, "objectSid":1}):
-            infos = self.sd_table.find_one({"sd_id":r["nTSecurityDescriptor"]},{"sd_value.Owner":1})
-            t.add([SID2StringFull(r["objectSid"],self.guid), SID2StringFull(infos["sd_value"]["Owner"], self.guid)])
+            req.update({"name": {"$regex": account, "$options": "-i"}})
+        for r in self.datatable.find(req, {"nTSecurityDescriptor": 1, "objectSid": 1}):
+            infos = self.sd_table.find_one({"sd_id": r["nTSecurityDescriptor"]}, {"sd_value.Owner": 1})
+            t.add([SID2StringFull(r["objectSid"], self.guid), SID2StringFull(infos["sd_value"]["Owner"], self.guid)])
         t.flush()
 
     def run(self, options, doc):
 
         if options.account_type not in self._types_:
-            account_type=self.datatable.find_one({"name":self._types_[0] })["DNT_col"]
+            account_type = self.datatable.find_one({"name": self._types_[0] })["DNT_col"]
         else:
-            account_type=self.datatable.find_one({"name":options.account_type})["DNT_col"]
+            account_type = self.datatable.find_one({"name": options.account_type})["DNT_col"]
 
         if options.created_since is not None:
             self.extract_field_since(doc, "whenCreated", options.created_since, [account_type])
