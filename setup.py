3b3bca34c4090e989241857691bbf317c9877a19
Phil <phil@secdev.org>
2014-04-24 03:10:50 +0200

    Added btamange tool

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     version = '0.3',
     packages=['bta', 'bta/tools', 'bta/backend', 'bta/miners', 'bta/formatters', 
               'libesedb'],
-    scripts = ['bin/btaminer', 'bin/btaimport', 'bin/btadiff', 'bin/btalist'],
+    scripts = ['bin/btaminer', 'bin/btaimport', 'bin/btadiff', 'bin/btalist', 'bin/btamanage'],
 
     # Metadata
     author = 'Philippe Biondi',
