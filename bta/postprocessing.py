airbus-seclab/bta

commit e3342c4e937ebc70bfdfd05775c0bd1b6b26bd03

Fix whitespace - no more tabs, trailing spaces.

bta/postprocessing.py
@@ -57,7 +57,7 @@ class PostProcessing(object):
         idSchema = idSchemaRec['DNT_col']
         for r in self.dt.find({"objectCategory": idSchema}):
             category.insert({"id":r["DNT_col"], "name":r["cn"]})
-    
+
     @PostProcRegistry.register()
     def rightsGuids(self):
         guid = self.options.backend.open_table("guid")
@@ -79,7 +79,7 @@ class PostProcessing(object):
         #
         for id in self.dt.find({"attributeID": {"$exists": 1}}):
             guid.insert({"id":id["attributeID"].lower(), "name":id["name"]})
-        
+
     @PostProcRegistry.register(depends={"category"})
     def domains(self):
         domains = self.options.backend.open_table("domains")
@@ -116,26 +116,26 @@ class PostProcessing(object):
         dnames.create_index("DNT_col")
         dnames.create_index("DName")
 
-	error = 0
+        error = 0
         for r in self.dt.find({"Ancestors_col":{"$exists":True}}):
-		dn=list()
-		for p in r["Ancestors_col"]:
-			try:
-				p=self.dt.find({"DNT_col":p}).limit(1)[0]
-			except:
-				error += 1
-				continue
-                        if p.get('name')=="$ROOT_OBJECT$\x00":
-                                continue
-                        if p.get('dc'):
-                                dn.append("DC=%s"%p['name'])
-                        elif p.get('cn'):
-                                dn.append("CN=%s"%p['name'])
-                        elif p.get('name'):
-                                dn.append("DC=%s"%p['name'])
-                dn.reverse()
-		dnames.insert({"name":r["name"], "DNT_col":r["DNT_col"], "DName":",".join(dn)})
-	print "NB ERRORS : %r" % error
+            dn=list()
+            for p in r["Ancestors_col"]:
+                try:
+                    p=self.dt.find({"DNT_col":p}).limit(1)[0]
+                except:
+                    error += 1
+                    continue
+                    if p.get('name')=="$ROOT_OBJECT$\x00":
+                        continue
+                    if p.get('dc'):
+                        dn.append("DC=%s"%p['name'])
+                    elif p.get('cn'):
+                        dn.append("CN=%s"%p['name'])
+                    elif p.get('name'):
+                        dn.append("DC=%s"%p['name'])
+            dn.reverse()
+            dnames.insert({"name":r["name"], "DNT_col":r["DNT_col"], "DName":",".join(dn)})
+        print "NB ERRORS : %r" % error
 
 
     @PostProcRegistry.register()
@@ -159,7 +159,7 @@ class PostProcessing(object):
 def main():
     import optparse
     parser = optparse.OptionParser()
-    
+
     parser.add_option("-C", dest="connection",
                       help="Backend connection string. Ex: 'dbname=test user=john' for PostgreSQL or '[ip]:[port]:dbname' for mongo)", metavar="CNX")
     parser.add_option("-B", dest="backend_class", default="mongo",
@@ -170,16 +170,15 @@ def main():
 
     parser.add_option("--overwrite", dest="overwrite", action="store_true",
                       help="Delete tables that already exist in db")
-    
+
     options, args = parser.parse_args()
-    
+
     if options.connection is None:
         parser.error("Missing connection string (-C)")
-    
 
     backend_class = bta.backend.Backend.get_backend(options.backend_class)
     options.backend = backend_class(options)
-    
+
     with bta.dblog.DBLogEntry.dblog_context(options.backend) as options.dblog:
 
         pp = PostProcessing(options)
@@ -188,7 +187,7 @@ def main():
         else:
             pp.post_process_all()
         options.backend.commit()
-    
+
 
 if __name__ == "__main__":
     main()

bta/backend/__init__.py
@@ -20,7 +20,7 @@ class Backend(object):
 
     def commit(self):
         pass
-        
+
     def create_table(self):
         raise NotImplementedError("Backend.create_table()")
     def open_table(self):

bta/backend/mongo.py
@@ -3,7 +3,7 @@
 
 import pymongo
 import struct
-from bta.normalization import TypeFactory,Normalizer
+from bta.normalization import TypeFactory, Normalizer
 from bta.backend import Backend, BackendTable
 import bson.binary
 import bta.sd
@@ -12,7 +12,7 @@ import bta.tools.decoding
 import logging
 import functools
 import re
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 log = logging.getLogger("bta.backend.mongo")
 
@@ -30,7 +30,7 @@ class MongoNormalizer(Normalizer):
 
 class MongoTextNormalizer(MongoNormalizer):
     pass
-    
+
 class MongoIntNormalizer(MongoNormalizer):
     def normal(self, val):
         if -0x8000000000000000 <= val < 0x8000000000000000:
@@ -73,9 +73,9 @@ class MongoNTSecDesc(MongoNormalizer):
 class MongoSID(MongoNormalizer):
     def normal(self, val):
         if val:
-            return bta.tools.decoding.decode_sid(val,">")
+            return bta.tools.decoding.decode_sid(val, ">")
         return None
-    
+
 class MongoGUID(MongoNormalizer):
     def normal(self, val):
         if val:
@@ -105,7 +105,7 @@ class MongoUserAccountControl(MongoNormalizer):
         if val is not None:
             return bta.datatable.UserAccountControl(val).to_json()
         return None
-    
+
 class MongoSecurityDescriptor(MongoNormalizer):
     def normal(self, val):
         if val:
@@ -127,7 +127,7 @@ class MongoOID(MongoNormalizer):
 
 class MongoWindowsTimestamp(MongoNormalizer):
     def normal(self, val):
-        val=val^0xffffffffffffffff
+        val = val^0xffffffffffffffff
         try:
             return datetime.fromtimestamp(0)+timedelta(microseconds=(val/10))
         except:
@@ -135,15 +135,15 @@ class MongoWindowsTimestamp(MongoNormalizer):
 
 class MongoLogonHours(MongoNormalizer):
     def normal(self, val):
-        hours=''.join(bin(x)[2:].zfill(8)[::-1] for x in bytearray(val))
-        days=['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-        if len(hours)>0:
-            hours=hours[-1]+hours[:-1]
-            hours=re.findall('........................',hours)
+        hours = ''.join(bin(x)[2:].zfill(8)[::-1] for x in bytearray(val))
+        days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+        if len(hours) > 0:
+            hours = hours[-1]+hours[:-1]
+            hours = re.findall('........................', hours)
         else:
-            hours=['x', 'x', 'x', 'x', 'x', 'x', 'x'] 
+            hours = ['x', 'x', 'x', 'x', 'x', 'x', 'x']
         return dict(zip(days, hours))
-        
+
 class MongoWindowsEnlaspedTime(MongoNormalizer):
     def normal(self, val):
         try:
@@ -185,9 +185,9 @@ class MongoTypeFactory(TypeFactory):
     def UnknownType(self):
         return MongoUnknownNormalizer()
     def Ancestors(self):
-	    return MongoAncestors()
+        return MongoAncestors()
     def OID(self):
-	    return MongoOID()
+        return MongoOID()
     def WindowsTimestamp(self):
         return MongoWindowsTimestamp()
     def WindowsEnlapsedTime(self):
@@ -234,7 +234,7 @@ class MongoTable(BackendTable):
         for c in columns:
             if c.index:
                 self.create_index(c.name)
-        
+
     def insert(self, values):
         return self.col.insert(values)
 
@@ -242,7 +242,7 @@ class MongoTable(BackendTable):
         return self.col.update(*args)
 
     def insert_fields(self, values):
-        d = {name:norm.normal(v) for (name,norm),v in zip(self.fields, values) if not norm.empty(v)}
+        d = {name:norm.normal(v) for (name, norm), v in zip(self.fields, values) if not norm.empty(v)}
         return self.insert(d)
 
     def count(self):
@@ -258,10 +258,10 @@ class MongoTable(BackendTable):
 class Mongo(Backend):
     def __init__(self, options, connection=None):
         Backend.__init__(self, options, connection)
-        ip,port,self.dbname,_ = (self.connection+":::").split(":",3)
+        ip, port, self.dbname, _ = (self.connection+":::").split(":", 3)
         ip = ip if ip else "127.0.0.1"
         port = int(port) if port else 27017
-        self.cnxstr = (ip,port)
+        self.cnxstr = (ip, port)
         self.cnx = pymongo.Connection(*self.cnxstr)
         self.db = self.cnx[self.dbname]
 
@@ -271,4 +271,3 @@ class Mongo(Backend):
     def list_tables(self):
         return self.db.collection_names()
 
-    

bta/dblog.py
@@ -3,6 +3,11 @@
 # This file is part of the BTA toolset
 # (c) EADS CERT and EADS Innovation Works
 
+"""
+Stores action logs to the "log" table in backend.
+Used for long-running operations (ex. btaimport).
+Automatically action status (exception occurred, interrupted, success).
+"""
 
 import sys
 import datetime
@@ -25,7 +30,7 @@ class DBLogEntry(object):
         except KeyboardInterrupt:
             dblog.update_entry("Interrupted by user (Ctrl-C)")
             raise
-        except Exception,e:
+        except Exception, e:
             dblog.update_entry("ERROR: %s" % e)
             raise
         else:
@@ -40,7 +45,7 @@ class DBLogEntry(object):
             actions = [],
             )
         self.entry_id = self.log.insert(e)
-        
+
     def update_entry(self, action):
         act = dict(
             date = datetime.datetime.now(),

bta/diff.py
@@ -7,7 +7,7 @@
 import bta.backend.mongo
 
 DEFAULT_IGNORE_LIST = {'whenChanged', 'replPropertyMetaData', 'dSCorePropagationData',
-                       'nTSecurityDescriptor', 'dnsRecord', 'uSNChanged', 'Ancestors_col', 
+                       'nTSecurityDescriptor', 'dnsRecord', 'uSNChanged', 'Ancestors_col',
                        'recycle_time_col', 'cnt_col', 'time_col', 'PDNT_col', 'dNSTombstoned',
                        }
 
@@ -27,9 +27,9 @@ class TableDiff(object):
         print "Starting diffing %s" % self.tablename
         print "---------------"
         icA = icB = None
-        
+
         total = old = new = diff = readA = readB = 0
-        
+
         while True:
             total += 1
             if icA is None:
@@ -48,16 +48,16 @@ class TableDiff(object):
                 else:
                     readB += 1
                     icB = rB[self.indexcol]
-                    
+
             if (icA is None) and (icB is None):
                 break
 
             if icB is None or icA is not None and icA < icB:
-                print "A ,%i: [%s]" % (icA, rA.get("name",""))
+                print "A, %i: [%s]" % (icA, rA.get("name", ""))
                 icA = None
                 old += 1
             elif icA is None or icA > icB:
-                print " B,%i: [%s]" % (icB, rB.get("name",""))
+                print "B, %i: [%s]" % (icB, rB.get("name", ""))
                 icB = None
                 new += 1
             else:
@@ -66,14 +66,14 @@ class TableDiff(object):
                 AnotB = sA-sB
                 BnotA = sB-sA
                 ABdiff = [k for k in sA&sB if rA[k] != rB[k]]
-                nameA, nameB = rA.get("name",""), rB.get("name","")
-                name = nameA if nameA == nameB else "A:[%s]/B:[%s]" % (nameA,nameB)
+                nameA, nameB = rA.get("name", ""), rB.get("name", "")
+                name = nameA if nameA == nameB else "A:[%s]/B:[%s]" % (nameA, nameB)
                 if AnotB or BnotA or ABdiff:
-                    descr = ["-%s" % k for k in AnotB]+["+%s" % k for k in BnotA]+["*%s[%r=>%r]" % (k,repr(rA[k])[:20],repr(rB[k])[:20]) for k in ABdiff]
-                    print "AB,%i: [%s] %s" % (icA, name, ", ".join(descr))
+                    descr = ["-%s" % k for k in AnotB]+["+%s" % k for k in BnotA]+["*%s[%r=>%r]" % (k, repr(rA[k])[:20], repr(rB[k])[:20]) for k in ABdiff]
+                    print "AB, %i: [%s] %s" % (icA, name, ", ".join(descr))
                     diff += 1
                 icA = icB = None
-                
+
         print "---------------"
         print "Table [%s]: %i records checked, %i disappeared, %i appeared, %i changed" % (self.tablename, total, old, new, diff)
         print "==============="
@@ -84,7 +84,7 @@ class TableDiff(object):
 def main():
     import optparse
     parser = optparse.OptionParser()
-    
+
     parser.add_option("--CA", dest="connectionA",
                       help="Backend A connection string. Ex: 'dbname=test user=john' for PostgreSQL or '[ip]:[port]:dbname' for mongo)", metavar="CNX")
     parser.add_option("--CB", dest="connectionB",
@@ -95,10 +95,9 @@ def main():
     parser.add_option("--BB", dest="backend_classB", default="mongo",
                       help="database B backend (amongst: %s)" % (", ".join(bta.backend.Backend.backends.keys())))
 
-    
     parser.add_option("--only", dest="only", default="",
                       help="Diff only TABLENAME", metavar="TABLENAME")
-    
+
     parser.add_option("-X", "--ignore-field", dest="ignore_list", action="append", default=[],
                       help="Add a field name to be ignored", metavar="FIELD")
     parser.add_option("-A", "--consider-field", dest="consider_list", action="append", default=[],
@@ -109,12 +108,11 @@ def main():
 
     options, args = parser.parse_args()
 
-    
     if options.connectionA is None:
         parser.error("Missing connection string A (--CA)")
     if options.connectionA is None:
         parser.error("Missing connection string B (--CB)")
-    
+
     options.ignore_list = set(options.ignore_list)
     options.consider_list = set(options.consider_list)
     if options.ignore_defaults:
@@ -123,12 +121,12 @@ def main():
 
     backend_classA = bta.backend.Backend.get_backend(options.backend_classA)
     options.backendA = backend_classA(options, options.connectionA)
-    
+
     backend_classB = bta.backend.Backend.get_backend(options.backend_classB)
     options.backendB = backend_classB(options, options.connectionB)
-    
-    for tablename,otherval,indexcol in [ ("sd_table", ["sdtable", "sd_table", "sd"], "sd_id"),
-                                         ("datatable", ["datatable", "data"], "DNT_col"), 
+
+    for tablename, otherval, indexcol in [("sd_table", ["sdtable", "sd_table", "sd"], "sd_id"),
+                                         ("datatable", ["datatable", "data"], "DNT_col"),
                                          ]:
         if not options.only or options.only.lower() in otherval:
             differ = TableDiff(options, tablename, indexcol)

bta/docstruct.py
@@ -29,7 +29,7 @@ class DocPart(object):
 
     def flush(self):
         self.parent.flush()
-    
+
     def to_json(self):
         content = [ (c.to_json() if hasattr(c,"to_json") else c) for c in self.content ]
         return { "name":self.name, "type": self._type_, "content": content }
@@ -139,7 +139,7 @@ class Table(DocPart):
 class List(DocPart):
     _type_ = "list"
     list_level = 0
-    
+
     def format_content(self, c):
         return "%s%s %s\n" % (" "*self.list_level,
                               ["*","+","-"][self.list_level%3],
@@ -202,8 +202,7 @@ if __name__ == "__main__":
     s2.add("unpushed content of sub2..1"); w()
     s2.add("unpushed content of sub2..2"); w()
     s2.add("unpushed content of sub2..3"); w()
-    
+
     d.finish_stream()
-    
 
 #    d.to_text_file(sys.stdout)

bta/formatters/rest.py
@@ -27,7 +27,7 @@ def format_table(table,  width_hint=200):
     hhfmt = ldcross + ( dcross.join(["%%-%is"]*len(tl)) % tuple(tl) ) + rdcross
     hhline = hhfmt % tuple(["="*l for l in tl])
     return fmt, hline, hhline
-    
+
 
 
 @Formatter.register

bta/miner.py
@@ -17,7 +17,7 @@ log = logging.getLogger("bta.miner")
 class categories(object):
     def __init__(self, ct):
         for entry in ct.find():
-            setattr(self, entry['name'].lower().replace('-','_'), int(entry['id']))
+            setattr(self, entry['name'].lower().replace('-', '_'), int(entry['id']))
 
 
 class MinerRegistry(Registry):
@@ -33,7 +33,7 @@ class Miner(object):
 
     @classmethod
     def create_arg_parser(cls):
-        
+
         parser = argparse.ArgumentParser()
 
         parser.add_argument("-C", dest="connection",
@@ -51,7 +51,6 @@ class Miner(object):
                             help="output file", metavar="FILENAME")
         parser.add_argument("-e", "--encoding", dest="encoding", default="utf8",
                             help="output encoding. Default: utf8", metavar="ENCODING")
-        
 
         subparsers = parser.add_subparsers(dest='miner_name', help="Miners")
         for miner in MinerRegistry.itervalues():
@@ -63,21 +62,21 @@ class Miner(object):
     @classmethod
     def create_arg_subparser(cls, parser):
         pass
-        
+
     @classmethod
     def main(cls):
         parser = cls.create_arg_parser()
         options = parser.parse_args()
-        
+
         if options.connection is None:
             parser.error("Missing connection string (-C)")
-    
+
         logging.basicConfig(level=logging.INFO,
                             format="%(levelname)-5s: %(message)s")
 
         backend_type = bta.backend.Backend.get_backend(options.backend_type)
         options.backend = backend_type(options)
-        
+
         if not options.output_type:
             options.live_output = True
 
@@ -88,7 +87,7 @@ class Miner(object):
         else:
             try:
                 m.assert_consistency()
-            except AssertionError,e:
+            except AssertionError, e:
                 log.error("Consistency check failed: %s" %e)
                 raise SystemExit()
 
@@ -146,7 +145,7 @@ class Miner(object):
         assert cnt > 0, "no record with [%s] attribute in [%s]" % (field, table.name)
     @classmethod
     def assert_field_type(cls, table, field, *types):
-        r = table.find_one({field : {"$exists":True}},{field:True})
+        r = table.find_one({field : {"$exists":True}}, {field:True})
         if r is not None:
             vtype = type(r[field])
             assert vtype in types, "unexpected type for value of attribute [%s] in table [%s] (got %r, wanted %r)" % (field, table.name, vtype, types)

commit 360a64de214bb9113bffe026a8d5f0bdc1106de6

Fixed dname postprocessing bug introduced when cleaning in e3342c4 then 5ab6a7e

bta/postprocessing.py
@@ -124,6 +124,14 @@ class PostProcessing(object):
                 except:
                     error += 1
                     continue
+                if p.get('name')=="$ROOT_OBJECT$\x00":
+                    continue
+                if p.get('dc'):
+                    dn.append("DC=%s"%p['name'])
+                elif p.get('cn'):
+                    dn.append("CN=%s"%p['name'])
+                elif p.get('name'):
+                    dn.append("DC=%s"%p['name'])
             dn.reverse()
             dnames.insert({"name":r["name"], "DNT_col":r["DNT_col"], "DName":",".join(dn)})
         print "NB ERRORS : %r" % error