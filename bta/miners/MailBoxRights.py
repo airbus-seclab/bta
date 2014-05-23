airbus-seclab/bta

commit cb065789a4e90017f8c9e5280a8882707547a317

Several fixes from pylint

bta/miners/MailBoxRights.py
@@ -31,13 +31,12 @@ class MailBoxRights(Miner):
                         continue
                     truc['SID'] = self.get_sid(truc['SID'])
                     aces.append(truc)
-            except Exception, e:
-                pass
+            except Exception,e:
+                log.warning(e)
         return aces
 
     def run(self, options, doc):
         userAccess = {}     # Permissions per User
-        mailboxAccess = {}  # Permissions per mailbox
         match = ""
         if options.userid:
             match =  { "objectSid": options.user }
@@ -48,22 +47,20 @@ class MailBoxRights(Miner):
             exit(1)
 
         mailboxes=self.datatable.find_one(match)
-        for mbox in mailboxes:
-            userMailboxCN = mailboxes['cn']
-            userMailBoxSecurityDescriptor = mailboxes['msExchMailboxSecurityDescriptor']
-            aces = self.getSecurityDescriptor(userMailBoxSecurityDescriptor)
-            if not aces:
-                continue
-            for ace in aces:
-                rules=[]
-                for key,val in ace['AccessMask'].items():
-                    if not val:
-                        continue
-                    rules.append("+%s" % key)
+        userMailboxCN = mailboxes['cn']
+        userMailBoxSecurityDescriptor = mailboxes['msExchMailboxSecurityDescriptor']
+        aces = self.getSecurityDescriptor(userMailBoxSecurityDescriptor)
+        if not aces:
+            continue
+        for ace in aces:
+            rules=[]
+            for key,val in ace['AccessMask'].items():
+                if not val:
+                    continue
+                rules.append("+%s" % key)
 
-                for rule in rules:
-                    userAccess[ace['SID']] =  userAccess.get(ace['SID'], {})
-                    userAccess[ace['SID']][userMailboxCN] = rules
+            userAccess[ace['SID']] =  userAccess.get(ace['SID'], {})
+            userAccess[ace['SID']][userMailboxCN] = rules
 
 
         s = doc.create_subsection("Who can accessed to this Mailbox")

bta/diff.py
@@ -28,7 +28,7 @@ class TableDiff(object):
         print "---------------"
         icA = icB = None
 
-        total = old = new = diff = readA = readB = 0
+        total = old = new = diff = _readA = _readB = 0
 
         while True:
             total += 1
@@ -38,7 +38,7 @@ class TableDiff(object):
                 except StopIteration:
                     pass
                 else:
-                    readA += 1
+                    _readA += 1
                     icA = rA[self.indexcol]
             if icB is None:
                 try:
@@ -46,7 +46,7 @@ class TableDiff(object):
                 except StopIteration:
                     pass
                 else:
-                    readB += 1
+                    _readB += 1
                     icB = rB[self.indexcol]
 
             if (icA is None) and (icB is None):

bta/formatters/csvzip.py
@@ -40,7 +40,7 @@ class CSVZIP(Formatter):
     def add_table(self, name, table):
         self._add_csv(name+".csv", [map(utf8,x) if x is not None else [""] for x in table])
 
-    def add_list(self, name, lst):
+    def add_list(self, name, lvl, lst):
         self._add_csv(name+".csv", [([""]*lvl+[x]) for lvl,x in lst])
 
     def add_section(self, section_name, lvl):

bta/formatters/excel.py
@@ -10,7 +10,6 @@ else:
     from bta.formatters import Formatter
     import StringIO
     from collections import defaultdict
-    import re
 
     from openpyxl.workbook import Workbook
     from openpyxl.styles import Style, Color, Font, Border, Side, PatternFill, fills, borders

bta/manage.py
@@ -5,7 +5,6 @@ import logging
 from bta.tools.registry import Registry
 from bta.backend import Backend
 from bta.tools.ask import ask
-import bta.backend.mongo
 
 log = logging.getLogger("bta.manage")
 

bta/miner.py
@@ -63,7 +63,7 @@ class Miner(object):
 
         parser.add_argument("--module", "-m", action="append", default=[])
 
-        globaloptions, other = parser.parse_known_args()
+        globaloptions,_ = parser.parse_known_args()
 
         logging.basicConfig(level=logging.INFO,
                             format="%(levelname)-5s: %(message)s")
@@ -71,7 +71,7 @@ class Miner(object):
         for m in globaloptions.module:
             imp = pkgutil.get_loader(m)
             if not imp:
-                preparser.error("Could not find module [%s]" % m)
+                parser.error("Could not find module [%s]" % m)
             mod = imp.load_module(m)
             if hasattr(mod, "import_all"):
                 mod.import_all()
@@ -187,6 +187,7 @@ class Miner(object):
 
 
 class MinerGroup(Miner):
+    # pylint: disable=abstract-method
     pass
 
 

bta/miners/DNTree.py
@@ -76,7 +76,6 @@ class DNTree(Miner):
                                 doc.add(u"%s:%s"%(str(key), str(value)))
 
         def find_ACE(node):
-            ace=list()
             id_sd = node.get('nTSecurityDescriptor')
             sd = self.sd_table.find({"sd_id":id_sd}).limit(1)[0]
             return sd

bta/miners/tools.py
@@ -33,7 +33,6 @@ class Family(object):
 
         def find_rec(node, offspring, datatable, rec):
             childs=Family.find_childs(node, datatable)
-            id_dict=needed(node)
             for s in childs:
                 id_new_node=needed(s)
                 if (len(Family.find_childs(s, datatable))==0 or rec==0):

commit fb05e058919bcf93675880313972e2342c9570f4

Fix bugs introduced in cb06578

bta/miners/MailBoxRights.py
@@ -2,6 +2,8 @@
 # (c) Airbus Group CERT, Airbus Group Innovations and Airbus DS CyberSecurity
 
 from bta.miner import Miner
+import logging
+log = logging.getLogger("bta.miners.MailBoxRights")
 
 
 @Miner.register
@@ -50,8 +52,6 @@ class MailBoxRights(Miner):
         userMailboxCN = mailboxes['cn']
         userMailBoxSecurityDescriptor = mailboxes['msExchMailboxSecurityDescriptor']
         aces = self.getSecurityDescriptor(userMailBoxSecurityDescriptor)
-        if not aces:
-            continue
         for ace in aces:
             rules=[]
             for key,val in ace['AccessMask'].items():