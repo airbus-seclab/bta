fd810c727b17fcc9aa80cec68627cc5e8be6d22a
phil <phil@secdev.org>
2014-06-20 17:46:56 +0200

    Added test_miners.py

---
+++ test/test_miners.py
@@ -0,0 +1,44 @@
+# This file is part of the BTA toolset
+# (c) Airbus Group CERT, Airbus Group Innovations and Airbus DS CyberSecurity
+
+import pytest
+import bta.miner
+import bta.miners
+import bta.backend
+import argparse
+
+bta.miners.import_all()
+
+
+
+class FakeBackend(bta.backend.Backend):
+    def open_table(self, name):
+        return FakeTable(self.options, None, name)
+
+class FakeTable(bta.backend.BackendTable):
+    def count(self):
+        return 1
+    def find(self, *args, **kargs):
+        return {}
+
+@pytest.fixture(scope="module")
+def fake_options():
+    return argparse.Namespace()
+
+@pytest.fixture(scope="module")
+def fake_backend(fake_options):
+    return FakeBackend(fake_options, "")
+
+
+
+def test_all_miners_create_subparser():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='miner_name', help="Miners")
+    for miner in bta.miner.MinerRegistry.itervalues():
+        p = subparser.add_parser(miner._name_, help=miner._desc_)
+        miner.create_arg_subparser(p)
+
+
+def test_all_miners_instantiate(fake_backend, fake_options):
+    for miner in bta.miner.MinerRegistry.itervalues():
+        miner(fake_backend)
