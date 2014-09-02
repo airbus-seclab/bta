ca71a0e086cba1a55fdcc2b9c4c49f7ef1b3843b
phil <phil@secdev.org>
2014-09-02 17:15:07 +0200

    Modified expression for better expressivity and to add ability to work on flags

--- bta/tools/expr.py
+++ bta/tools/expr.py
@@ -1,20 +1,64 @@
 # This file is part of the BTA toolset
 # (c) Airbus Group CERT, Airbus Group Innovations and Airbus DS CyberSecurity
 
-    
+
+class Field(object):
+    def __init__(self, name):
+        self.name = name
+    def present(self):
+        return Cond(self, "_present_")
+    def absent(self):
+        return Cond(self, "_absent_")
+
+    def flag_on(self, flags):
+        return Cond(self, "_flagon_", flags)
+    def flag_off(self, flags):
+        return Cond(self, "_flagoff_", flags)
+
+    def __eq__(self, other):
+        return Cond(self, "_eq_", other)
+    def __ne__(self, other):
+        return Cond(self, "_ne_", other)
+    def __repr__(self):
+        return "<Field: %r>" % self.name
+    def build(self, builder):
+        return builder._field_(self.name)
+
+class Cond(object):
+    def __init__(self, op1, op, op2=None):
+        self.op1 = op1
+        self.op = op
+        self.op2 = op2
+
+    def __and__(self, other):
+        return Expr(self, "_and_", other)
+    def __or__(self, other):
+        return Expr(self, "_or_", other)
+
+
+    def __repr__(self):
+        if self.op2 is None:
+            return "<Cond: %r %s>" % (self.op1, self.op)
+        return "<Cond: %r %s %r>" % (self.op1, self.op, self.op2)
+    def build(self, builder):
+        op1 = self.op1.build(builder) if hasattr(self.op1, "build") else self.op1
+        if self.op2 is None:
+            return getattr(builder, self.op)(op1)
+        op2 = self.op2.build(builder) if hasattr(self.op2, "build") else self.op2
+        return getattr(builder, self.op)(op1, op2)
+
+
 class Expr(object):
     def __init__(self, op1, op, op2):
         self.op1 = op1
         self.op = op
         self.op2 = op2
+
     def __and__(self, other):
         return Expr(self, "_and_", other)
     def __or__(self, other):
         return Expr(self, "_or_", other)
-    def __eq__(self, other):
-        return Expr(self, "_eq_", other)
-    def __ne__(self, other):
-        return Expr(self, "_ne_", other)
+
     def __repr__(self):
         return "<Expr: (%r %s %r)>" % (self.op1, self.op, self.op2)
     def build(self, builder):
@@ -22,20 +66,6 @@ class Expr(object):
         op2 = self.op2.build(builder) if hasattr(self.op2, "build") else self.op2
         
         return getattr(builder, self.op)(op1, op2)
-
-class Field(object):
-    def __init__(self, name, flags=None):
-        self.name = name
-        self.flags = flags
-    def __eq__(self, other):
-        return Expr(self, "_eq_", other)
-    def __ne__(self, other):
-        return Expr(self, "_ne_", other)
-    def __repr__(self):
-        return "<Field: %r>" % self.name
-    def build(self, builder):
-        return builder._field_(self.name, self.flags)
-
 
 
 class Builder(object):
@@ -51,10 +81,7 @@ class FormulaBuilder(Builder):
         return "<formula builder: %s>" % self
 
     @classmethod
-    def _field_(cls, name, flags):
-        n = repr(name)
-        if flags:
-            n += "."+repr(flags)
+    def _field_(cls, name):
         return cls(repr(name))
     @classmethod
     def _and_(cls, op1, op2):
@@ -64,10 +91,23 @@ class FormulaBuilder(Builder):
         return cls("(%s) || (%s)" % (op1, op2))
     @classmethod
     def _eq_(cls, op1, op2):
-        return cls("(%s) == (%s)" % (op1, op2))
+        return cls("%s == %s" % (op1, op2))
     @classmethod
     def _ne_(cls, op1, op2):
-        return cls("(%s) != (%s)" % (op1, op2))
+        return cls("%s != %s" % (op1, op2))
+    @classmethod
+    def _flagon_(cls, op1, op2):
+        return cls("%s & %s != 0" % (op1, op2))
+    @classmethod
+    def _flagoff_(cls, op1, op2):
+        return cls("%s & %s == 0" % (op1, op2))
+
+    @classmethod
+    def _present_(cls, op1):
+        return cls("%s exists" % op1)
+    @classmethod
+    def _absent_(cls, op1):
+        return cls("%s does not exist" % op1)
 
 
 
