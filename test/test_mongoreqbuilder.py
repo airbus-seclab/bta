fd8f54e70432b57597fa562e31a086c67d69a5c9
phil <phil@secdev.org>
2014-12-11 13:19:01 +0100

    Fix reqmongobuilder test

--- test/test_mongoreqbuilder.py
+++ test/test_mongoreqbuilder.py
@@ -5,33 +5,33 @@ from bta.tools.expr import *
 
 def test_eq():
     r = Field("toto")=="tata"
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "toto": "tata" }
 
 def test_ne():
     r = Field("toto")!="tata"
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "toto":{"$ne":"tata" } }
 
 def test_present():
     r = Field("toto").present()
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "toto":{"$exists":True } }
 
 def test_absent():
     r = Field("toto").absent()
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "toto":{"$exists":False } }
 
 def test_and():
     r = (Field("toto")=="tata") & (Field("titi")!="tutu")
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "toto": "tata",
                    "titi":{"$ne":"tutu" } }
 
 def test_and():
     r = (Field("toto")=="tata") | (Field("titi")!="tutu")
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "$or" : [ { "toto": "tata" },
                              { "titi":{"$ne":"tutu" } } ] }
 
@@ -39,10 +39,10 @@ def test_and():
 
 def test_flags():
     r = Field("userAccountControl").flag_on("normalAccount")
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "userAccountControl.flags.normalAccount" : True }
     r = Field("userAccountControl").flag_off("passwordExpired")
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { "userAccountControl.flags.passwordExpired" : False }
 
 
@@ -50,7 +50,7 @@ def test_all():
     r = (Field("toto")=="tata") | (Field("titi")!="tutu")
     r &= Field("foo") == "bar"
     r |= Field("userAccountControl").flag_on("smartcardRequired")
-    mr = r.build(MongoReqBuilder)
+    mr = r.build(MongoReqBuilder())
     assert mr == { 
         "$or" : [
             { "foo": "bar",
